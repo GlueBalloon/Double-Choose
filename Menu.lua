157d9380474c4fcb98e40547aa7893156da389c9
Jesse Wonder Clark <jwc@jessewonderclark.com>
2021-05-09 14:14:41 -0400

    load kids' face images to menu

--- Menu.lua
+++ Menu.lua
@@ -1,6 +1,10 @@
 menuImages = {}
 menuImages.menu = readImage(asset.doubleMenuBackground)
+menuImages.charlotte = readImage(asset.doubleMenuCharlotte)
+menuImages.rosie = readImage(asset.doubleMenuRosie)
     
 function mainMenu()
     drawBackground(menuImages.menu)
+    simpleImage("menuRosie", menuImages.rosie, HEIGHT * 0.0014)
+    simpleImage("menuCharlotte", menuImages.charlotte)
 end
