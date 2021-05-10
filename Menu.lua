9a4db3562ab043054acfccc91bd19cde65fe926c
Jesse Wonder Clark <jwc@jessewonderclark.com>
2021-05-10 12:28:10 -0400

    Size and place girl faces correctly

--- Menu.lua
+++ Menu.lua
@@ -2,9 +2,13 @@ menuImages = {}
 menuImages.menu = readImage(asset.doubleMenuBackground)
 menuImages.charlotte = readImage(asset.doubleMenuCharlotte)
 menuImages.rosie = readImage(asset.doubleMenuRosie)
-    
+menuratio = 0.0014
+    parameter.number("menuratio", 0.0012, 0.002, menuratio, function()
+    print(menuratio)
+end)
+parameter.watch("menuratio")
 function mainMenu()
     drawBackground(menuImages.menu)
-    simpleImage("menuRosie", menuImages.rosie, HEIGHT * 0.0014)
-    simpleImage("menuCharlotte", menuImages.charlotte)
+    simpleImage("menuRosie", menuImages.rosie, HEIGHT * 0.0012969697)
+    simpleImage("menuCharlotte", menuImages.charlotte, HEIGHT * 0.0012969697)
 end
